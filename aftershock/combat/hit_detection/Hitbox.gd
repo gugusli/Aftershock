4b49c4e87271fe57b79478a77749813ba1e9c47e
guguslu <diegoagustinaragon@gmail.com>
2026-01-25 15:00:57 -0300

    Delete aftershock directory

--- aftershock/combat/hit_detection/Hitbox.gd
+++
@@ -1,6 +0,0 @@
-extends Area2D
-
-@onready var damageable := get_parent().get_node("Damageable")
-
-func receive_hit(amount: int) -> void:
-	damageable.take_damage(amount)
--- aftershock/levels/Arena.gd
+++
@@ -1,7 +0,0 @@
-extends Node2D
-
-@onready var player = $Player
-@onready var hud = $HUD
-
-func _ready():
-	hud.connect_player(player)
