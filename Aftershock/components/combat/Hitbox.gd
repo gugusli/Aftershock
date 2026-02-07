bc645bd6f4fcb034b57dd95e02b1d37ebf0d0df8
guguslu <diegoagustinaragon@gmail.com>
2026-02-07 14:45:48 -0300

    Delete Aftershock directory

--- Aftershock/components/combat/Hitbox.gd
+++
@@ -1,7 +0,0 @@
-extends Area2D
-
-@onready var damageable := get_parent().get_node_or_null("Damageable")
-
-func receive_hit(amount: float) -> void:
-	if damageable:
-		damageable.take_damage(amount)
